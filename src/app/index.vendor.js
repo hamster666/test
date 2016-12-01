e9db856f84cc5d71856aae97b7cfe206daf922c0
Laura Hampson <lhampson@andigital.com>
2016-12-01 15:04:58 +0000

    working state

--- src/app/index.vendor.js
+++ src/app/index.vendor.js
@@ -5,14 +5,11 @@ import "angular";
 
 import "angular-ui-router";
 
-
 import "oclazyload";
 
 import "angular-sanitize";
 
 import "angular-aria";
-
-
 
 // local scripts
 //import "../assets/js/...";
